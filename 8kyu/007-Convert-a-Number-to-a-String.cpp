bf11fe743e47ea8c5519e0b6c610855276560bfd
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-02-10 16:14:07 +0700

    Create 8kyu\007-Convert-a-Number-to-a-String.cpp

---
+++ 8kyu/007-Convert-a-Number-to-a-String.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string number_to_string(int num) {
+  // your code here
+  return to_string(num);
+}
+
+int main(){
+    cout<<number_to_string(5);
+    return 0;
+}
