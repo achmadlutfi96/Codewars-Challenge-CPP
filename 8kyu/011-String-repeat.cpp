665ffbcf7ff5acf2a0215f4314a40a229d777862
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-11 19:23:42 +0700

    Create 8kyu\011-String-repeat.cpp

---
+++ 8kyu/011-String-repeat.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string repeat_str(size_t repeat, const string& str) {
+    string res="";
+    for(int i=0;i<repeat; i++){
+        res += str;
+    }
+    return res;
+}
+
+int main(){
+    cout<<repeat_str(3, "*");
+    return 0;
+}
