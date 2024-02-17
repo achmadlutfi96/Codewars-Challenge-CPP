189b6514b7192d844ac6737596f190c259acbdc5
Achmad Lutfi <alhacker1996@gmail.com>
2024-02-17 21:15:31 +0700

    Create 8kyu\024-Function-1-hello-world.cpp

---
+++ 8kyu/024-Function-1-hello-world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <string>
+
+// Write a function "greet" that returns "hello world!" of type std::string
+std::string greet(){
+    return "hello world!";
+}
+
+int main(){
+    std::cout<<greet();
+    return 0;
+}
