fdf3bb2694a3a1d01b4678768ef4b4876db275bb
Achmad Lutfi <alhacker1996@gmail.com>
2024-03-03 21:42:59 +0700

    Create 8kyu\046-You-Cant-Code-Under-Pressure-1.cpp

---
+++ 8kyu/046-You-Cant-Code-Under-Pressure-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdint>
+
+int32_t double_integer(int32_t n)
+{
+  // We need solution fast !!!
+  return n*2;
+}
+
+int main(){
+    std::cout<<double_integer(5);
+    return 0; 
+}
